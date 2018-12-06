de3630b17b683fbff76dec2a09d8a3a991e8101e
Walter Kimaro <wakyj07@gmail.com>
2018-12-06 17:29:17 +0300

    Add video remover

--- src/NewPost/NewPostVideo/index.jsx
+++ src/NewPost/NewPostVideo/index.jsx
@@ -59,9 +59,9 @@ class NewPostVideo extends React.Component {
         return ( 
             <div className={'ot-new-post-video ' + ( loading ? 'loading' : '' ) + ( is_temp ? ' is-temp' : '' )}>
                 <PostVideo video={video} />
-                {/* <button className="ot-btn action" onClick={this.props.onRemoveVideo}>
+                <button className="ot-btn action" onClick={this.props.onRemoveVideo}>
                     <svg viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>
-                </button> */}
+                </button>
             </div>
         );
     }
