b20a711670309b0a87b0a36c5e20cd83f57b5b7c
Walter Kimaro <wakyj07@gmail.com>
2019-05-01 17:39:22 +0300

    Tweak mobile navigation and add alerts nav item

--- src/App/MobileNav/index.jsx
+++ src/App/MobileNav/index.jsx
@@ -10,38 +10,46 @@ const MobileNav = ( props ) => {
             <button className={'ot-btn flat layout vertical center-center ' + ( page === 'home' ? 'active' : '' )} 
                 onClick={ () => onNavigateTo('home') }>
                 <svg viewBox="0 0 24 24"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
-                Home
+                <span>Home</span>
             </button>
 
             <button className={'ot-btn flat layout vertical center-center ' + ( page === 'woza' ? 'active' : '' )} 
                 onClick={ () => onNavigateTo('woza') }>
                 <svg viewBox="0 0 24 24"><polyline points="22 12 18 12 15 21 9 3 6 12 2 12"></polyline></svg>
-                Woza
+                <span>Woza</span>
             </button>
 
-            <button className={'ot-btn flat layout vertical center-center ' + ( page === 'discover' ? 'active' : '' )} 
+            {/* <button className={'ot-btn flat layout vertical center-center ' + ( page === 'discover' ? 'active' : '' )} 
                 onClick={ () => onNavigateTo('discover') }>
                 <svg viewBox="0 0 24 24"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
                 Discover
+            </button> */}
+
+            <button className={'ot-btn flat layout vertical center-center ' + ( page === 'alerts' ? 'active' : '' )} 
+                onClick={ () => onNavigateTo('alerts') }>
+                <svg viewBox="0 0 24 24"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg>
+                <span>Alerts</span>
             </button>
 
             <button className={'ot-btn flat layout vertical center-center ' + ( page === 'ochat' ? 'active' : '' )} 
                 onClick={ () => onNavigateTo('ochat') }>
                 <svg viewBox="0 0 24 24"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>
-                OChat
+                <span>OChat</span>
             </button>
 
-            <button className={'ot-btn flat layout vertical center-center ' + ( page === 'apps' ? 'active' : '' )} 
+            {/* <button className={'ot-btn flat layout vertical center-center ' + ( page === 'apps' ? 'active' : '' )} 
                 onClick={ () => onNavigateTo('apps') }>
                 <svg viewBox="0 0 24 24"><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
                 Apps
-            </button>
+            </button> */}
 
-            {/*
-            <button className="ot-btn action" onClick={ () => onNavigateTo('home') }>
-                <svg id="woza-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="#727A7E" d="M12 20.664a9.163 9.163 0 0 1-6.521-2.702.977.977 0 0 1 1.381-1.381 7.269 7.269 0 0 0 10.024.244.977.977 0 0 1 1.313 1.445A9.192 9.192 0 0 1 12 20.664zm7.965-6.112a.977.977 0 0 1-.944-1.229 7.26 7.26 0 0 0-4.8-8.804.977.977 0 0 1 .594-1.86 9.212 9.212 0 0 1 6.092 11.169.976.976 0 0 1-.942.724zm-16.025-.39a.977.977 0 0 1-.953-.769 9.21 9.21 0 0 1 6.626-10.86.975.975 0 1 1 .52 1.882l-.015.004a7.259 7.259 0 0 0-5.223 8.558.978.978 0 0 1-.955 1.185z"></path></svg>
+            
+            <button className={'ot-btn flat layout vertical center-center ' + ( page === 'profile' ? 'active' : '' )} 
+                onClick={ () => onNavigateTo('profile') }>
+                <svg viewBox="0 0 24 24"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+                <span>Profile</span>
             </button>
-            */}
+           
         </div>
     );
 }
