d89dae2fdfb7b0f6b614ea24dd80152105f4255e
Walter Kimaro <wakyj07@gmail.com>
2019-05-02 00:54:38 +0300

    Make Home page component a functional component

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,54 +1,50 @@
 import React from 'react';
 
-import logo from '../components/logo.png';
-
 import PostList from './PostList';
 import Header from '../components/Header';
 import OtPage from '../components/OtPage';
 import Menu from '../components/Menu';
 
-class Home extends React.Component {
-    render() { 
-        const { user, onViewProfile, onLogout, onViewUser } = this.props;
+import logo from '../components/logo.png';
 
-        return (
-            <OtPage>
-                <Header>
-                    <img className="ot-logo" src={logo} alt=""/>
-                    &emsp;
-                    <span className="ot-header-title">
-                        Otalk
-                    </span>
+const Home = ({ user, onViewProfile, onLogout, onViewUser }) => {
+    return (
+        <OtPage>
+            <Header>
+                <img className="ot-logo" src={logo} alt=""/>
+                &emsp;
+                <span className="ot-header-title">
+                    Otalk
+                </span>
 
-                    <span className="flex"></span>
+                <span className="flex"></span>
 
-                    <div className="ot-dp">
-                        <img src={user.dp} alt="" onClick={onViewProfile} />
-                    </div>
+                <div className="ot-dp">
+                    <img src={user.dp} alt="" onClick={onViewProfile} />
+                </div>
 
-                    <Menu className="ot-auth-user">
-                        <Menu.Trigger className="ot-btn flat layout center">
-                            <span className="ot-dp">
-                                <img src={user.dp} alt="" />
-                            </span>
-                            <p>&nbsp;&nbsp; { user.display_name }</p>
+                <Menu className="ot-auth-user">
+                    <Menu.Trigger className="ot-btn flat layout center">
+                        <span className="ot-dp">
+                            <img src={user.dp} alt="" />
+                        </span>
+                        <p>&nbsp;&nbsp; { user.display_name }</p>
 
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>
-                        </Menu.Trigger>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>
+                    </Menu.Trigger>
 
-                        <Menu.Options>
-                            {/* <Menu.Option onSelected={ onViewProfile }> Profile </Menu.Option> */}
-                            <Menu.Option onSelected={ onLogout }> Logout </Menu.Option>
-                        </Menu.Options>
-                    </Menu>
-                </Header>
+                    <Menu.Options>
+                        {/* <Menu.Option onSelected={ onViewProfile }> Profile </Menu.Option> */}
+                        <Menu.Option onSelected={ onLogout }> Logout </Menu.Option>
+                    </Menu.Options>
+                </Menu>
+            </Header>
 
-                <PostList
-                    user={ user }
-                    onViewUser={onViewUser} />
-            </OtPage> 
-        );
-    }
+            <PostList 
+                user={ user } 
+                onViewUser={onViewUser} />
+        </OtPage> 
+    );
 }
  
 export default Home;
