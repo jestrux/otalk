72ced2ac504ceb0cd4c81f688eb2d730be336641
Walter Kimaro <wakyj07@gmail.com>
2019-05-01 23:44:11 +0300

    Allow otPage to reveive styles

--- src/components/OtPage/index.jsx
+++ src/components/OtPage/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './ot-page.css';
 
-const OtPage = ({className , ...props}) => {
+const OtPage = ({className, style , ...props}) => {
     const bg = props.bg || '#ecf1f0';
     const padding = props.padding || '';
     const full = props.full || false;
     
     return (
-        <div id="otPage" className={className + ' ' + ( full ? 'full' : '' )} style={ { backgroundColor: bg, paddingTop: padding } }>
+        <div id="otPage" className={className + ' ' + ( full ? 'full' : '' )} style={ { ...style, backgroundColor: bg, paddingTop: padding } }>
             { props.children }
         </div>
     );
